38424a0dcb695d0e013587d3d52d4445ccea48bb
Isaac Rocha <isaacgeazy@gmail.com>
2023-02-03 11:02:39 -0300

    Finalizado.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var btn = document.getElementById('btn');
+array = []
 
 function converterMoneda() {
 
@@ -6,15 +7,14 @@ function converterMoneda() {
     var res = document.getElementById("resposta")
 
     if (caixa == "") {
-
         res.innerHTML = "é necessario preencher a caixa"
     } else {
         verificarRadio()
-        historicoDeConversao()
     }
 }
 
 function verificarRadio() {
+
     var caixa = document.querySelector("#text").value
     var radio = document.querySelector('input[name="conv"]:checked')
     var res = document.getElementById("resposta")
@@ -22,19 +22,30 @@ function verificarRadio() {
     if (radio.value == "RD") {
         let calculo = caixa / 5.06
         res.innerHTML = calculo.toFixed(2).replace('.', ',')
+        historicoDeConversao()
     } else if (radio.value == "DR") {
         let calculo = caixa * 5.06
         res.innerHTML = calculo.toFixed(2).replace('.', ',')
+        historicoDeConversao()
     } else {
         res.innerHTML = "é necessário escolher uma opção!"
     }
 }
 
 function historicoDeConversao() {
+
     var caixa = document.querySelector("#text").value
-    var array = []
+    var historico = document.getElementById('historico')
+
+    historico.innerHTML = ""
+    
     array.push(caixa)
-    console.log(array)
+
+    array.forEach(element => {
+        historico.insertAdjacentHTML('beforeend', `<p>$${element.replace('.', ',')}</p>`)
+
+    });
+
 }
 
 btn.addEventListener('click', converterMoneda)
